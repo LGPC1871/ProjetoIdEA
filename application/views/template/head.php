5d4ca90fedaa177ec4d1c33519eac5985478b93b
Luis Gustavo Pinheiro da Cunha <lgpc1871@gmail.com>
2020-04-26 18:19:26 -0300

    Movendo vendor e adicionando font-awesome local

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -6,6 +6,7 @@ $diretorio = base_url();
 <html lang="en">
 <head>
     <meta name="google-signin-client_id" content="482205672789-flab0s8c0o2q5844v4h1mqlgdagcmvuj.apps.googleusercontent.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <!--Head Scripts-->
     <?php if(isset($headScripts)): ?>
@@ -13,24 +14,22 @@ $diretorio = base_url();
             <script src="<?=$hscript?>"></script>
         <?php endforeach?>
     <?php endif ?>
-
-    <script type="text/javascript" src="<?=$diretorio?>/public/js/jquery.js"></script>
-    <script type="text/javascript" src="<?=$diretorio?>/public/js/bootstrap.js"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=$diretorio?>/public/css/bootstrap/bootstrap.css">
+    <base href="<?=$diretorio?>/">
+    <script type="text/javascript" src="public/js/jquery.js"></script>
+    <script type="text/javascript" src="public/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="public/css/bootstrap/bootstrap.css">
     <!--Font Awesome-->
-    <script src="https://kit.fontawesome.com/09f0e3d5de.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css">
     <!--Custom CSS stylesheet-->
-    <link rel="stylesheet" href="<?=$diretorio?>/public/css/custom/style.css">
-    <!--<link rel="stylesheet" href="<?=$diretorio?>/public/css/custom/login.css">-->
-    <link rel="stylesheet" href="<?=$diretorio?>/public/css/custom/header.css">
-    <link rel="stylesheet" href="<?=$diretorio?>/public/css/custom/main.css">
-    <link rel="stylesheet" href="<?=$diretorio?>/public/css/custom/footer.css">
+    <link rel="stylesheet" href="public/css/custom/style.css">
+    <link rel="stylesheet" href="public/css/custom/header.css">
+    <link rel="stylesheet" href="public/css/custom/main.css">
+    <link rel="stylesheet" href="public/css/custom/footer.css">
     
     <!--Custom Unique CSS stylesheet-->
     <?php if(isset($styles)): ?>
         <?php foreach($styles as $style_name): ?>
-            <?php $href = base_url() . "public/css/custom/" . $style_name ?>
+            <?php $href ="public/css/custom/" . $style_name ?>
             <link href="<?=$href?>" rel="stylesheet">
         <?php endforeach ?>
     <?php endif ?>
