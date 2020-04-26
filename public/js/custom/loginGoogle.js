ee605e45c0404b349c7690d59f539a1dc22784f1
Luis Gustavo Pinheiro da Cunha <lgpc1871@gmail.com>
2020-04-26 18:45:06 -0300

    Update loginGoogle.js

--- public/js/custom/loginGoogle.js
+++ public/js/custom/loginGoogle.js
@@ -1,16 +1,12 @@
 function onSignIn(googleUser) {
     var userToken = googleUser.getAuthResponse().id_token;
-    
     var userData = {
         userToken: userToken
     }
-
     sendUserTokenToBackend(userData);
 }
 
 function sendUserTokenToBackend(userData){
-  
-
     $.ajax({
         type: "POST",
         url: BASE_URL + 'user/googleAjaxLogin',
@@ -26,16 +22,3 @@ function sendUserTokenToBackend(userData){
         }
     })
 }
-/*
-  $.ajax({
-        'url': BASE_URL + 'user/googleAjaxLogin',
-        'type': 'POST',
-        'data': userData,
-        'success': function(json){
-            window.location = BASE_URL + "user/profile";
-        },
-        'error': function(){
-            console.log('triste...')
-        }
-      })
-      */
